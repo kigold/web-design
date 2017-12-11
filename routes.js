bc3f5ed0464cb8b0b9ce014428c72554ad6c5817
kingslee <kingsleybox@yahoo.com>
2017-12-11 17:08:36 +0100

    updated router.js

--- routes.js
+++ routes.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const db = require('../database/queries');
-const database = require('../database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
