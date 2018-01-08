3095c356c0aa9ff76447af39c4e0b87a8d9203eb
kingslee <kingsleybox@yahoo.com>
2018-01-08 14:22:10 +0100

    designed the Hotel page using Grid css

---
+++ public/web/js/date.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    console.log("date");
+    var checkin = $("#check-in");
+    var checkout = $("#check-out");
+    checkin.change(function() {
+        this.setAttribute(
+            "data-date",
+            moment(this.value, "YYYY-MM-DD")
+            .format( this.getAttribute("data-date-format") )
+        )
+    }).trigger("change")
+    checkout.change(function() {
+        this.setAttribute(
+            "data-date",
+            moment(this.value, "YYYY-MM-DD")
+            .format( this.getAttribute("data-date-format") )
+        )
+    }).trigger("change")
+});
